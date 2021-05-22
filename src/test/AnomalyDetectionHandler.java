3562f9e9ecb5e26275735d895d72415e8d71fd4d
linoy elimeleh <lino1998y@gmail.com>
2021-05-22 09:12:48 +0300

    running exam number 4

--- src/test/AnomalyDetectionHandler.java
+++ src/test/AnomalyDetectionHandler.java
@@ -1,33 +1,51 @@
 package test;
 
-
 import test.Commands.DefaultIO;
 import test.Server.ClientHandler;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class AnomalyDetectionHandler implements ClientHandler {
+
+    @Override
+    public void handleClient(InputStream inFromClient, OutputStream outToClient) {
+        SocketIO socket = new SocketIO(outToClient, inFromClient);
+        CLI cli = new CLI(socket);
+        cli.start();
+        cli.dio.write("bye");
+    }
 
     public class SocketIO implements DefaultIO {
 
+        Scanner in;
+        PrintWriter out;
+
+        public SocketIO(OutputStream outToClient, InputStream inFromClient) {
+            in = new Scanner(inFromClient);
+            out = new PrintWriter(outToClient);
+        }
+
         @Override
         public String readText() {
-            return null;
+            return in.nextLine();
         }
 
         @Override
         public void write(String text) {
-
+            out.print(text);
+            out.flush();
         }
 
         @Override
         public float readVal() {
-            return 0;
+            return in.nextFloat();
         }
 
         @Override
         public void write(float val) {
-
+            out.print(val);
+            out.flush();
         }
     }
-
-
 }
--- src/test/Server.java
+++ src/test/Server.java
@@ -1,27 +1,50 @@
 package test;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class Server {
+    volatile boolean stop;
 
-	public interface ClientHandler{
-		// define...
-	}
+    public interface ClientHandler {
+        void handleClient(InputStream inFromClient, OutputStream outToClient);
+    }
 
-	volatile boolean stop;
-	public Server() {
-		stop=false;
-	}
-	
-	
-	private void startServer(int port, ClientHandler ch) {
-		// implement here the server...
-	}
-	
-	// runs the server in its own thread
-	public void start(int port, ClientHandler ch) {
-		new Thread(()->startServer(port,ch)).start();
-	}
-	
-	public void stop() {
-		stop=true;
-	}
+    public Server() {
+        this.stop = false;
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+
+    private void startServer(int port, ClientHandler ch) {
+        try {
+            ServerSocket server = new ServerSocket(port);
+            while (!stop) {
+                server.setSoTimeout(1000);
+                try {
+                    Socket client = server.accept();
+                    InputStream in = client.getInputStream();
+                    OutputStream out = client.getOutputStream();
+                    ch.handleClient(in, out);
+                    in.close();
+                    out.close();
+                    client.close();
+                    stop();
+                } catch (IOException E) {
+                }
+            }
+            server.close();
+        } catch (SocketException e) {
+        } catch (IOException e1) {
+        }
+    }
+
+    // runs the server in its own thread
+    public void start(int port, ClientHandler ch) {
+        new Thread(() -> this.startServer(port, ch)).start();
+    }
 }
